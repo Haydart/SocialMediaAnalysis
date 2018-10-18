cdf44552b7c2e4098a8d4226743055a1c447fc61
Haydart <makowiecki.radoslaw@gmail.com>
2018-10-18 18:32:31 +0200

    Initial commit

---
+++ local_elections/twitter_data/acquire_data.py
@@ -0,0 +1,18 @@
+import twint
+
+
+def fetch_tweets(user, filename, search=None, since="2018-01-01", limit=1000):
+    config = twint.Config()
+    config.Username = user
+    if search:
+        config.Search = search
+    config.Since = since
+    config.Limit = limit
+    config.Store_csv = True
+    config.Custom = ["date", "time", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
+    config.Output = filename
+    twint.run.Search(config)
+
+
+for candidate in ['RozeckaPL', 'GoTracz', 'martalempart', 'MichalakJerzy', 'KatarzynaObara', 'SutrykJacek']:
+    fetch_tweets(candidate, candidate.lower() + '.csv')
